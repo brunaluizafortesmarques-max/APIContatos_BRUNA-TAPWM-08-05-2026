b84568830cb05fffdf40bd9b460bb4ab39613851
brunaluizafortesmarques-max <brunaluizafortesmarques@gmail.com>
2026-05-29 18:51:33 +0000

    fiz alt

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const conectarBanco = require('./config/db');
 const contatoRoutes = require('./routes/contatoRoutes');
+
 dotenv.config();
 const app = express();
+
 // Middleware para interpretar JSON no corpo das requisições
 app.use(express.json());
+
 // Rotas principais
 app.use('/api/contatos', contatoRoutes);
+
 // Conectar ao banco e iniciar o servidor
 conectarBanco();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Servidor rodando na porta ${PORT}'));
-// Aqui o que eu add a mais 
-const conectarBanco = require('./db');
-conectarBanco();
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
 const mongoose = require('mongoose');
 
 const usuarioSchema = new mongoose.Schema({
